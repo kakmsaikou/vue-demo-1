84835a119d2f539107ee5c8d0c92b18dd9515b12
英TacitusPie <kakmsaikou@gmail.com>
2022-08-12 15:29:00 +0800

    test GitHub

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :class="`app theme-${themeName}`">
+    <change-theme/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import changeTheme from '@/components/ChangeTheme'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {changeTheme},
+    data() {
+      return {
+        themeName: 'blue',
+      }
+    },
+    provide() {
+      return {
+        themeNameK: this.themeName,
+        turnColorK: this.turnColor
+      }
+    },
+    methods: {
+      turnColor() {
+        this.themeName === 'blue' ? this.themeName = 'red' : this.themeName = 'blue'
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .app.theme-blue button {
+    background-color: blue;
+    color: white;
+  }
+
+  .app.theme-red button {
+    background-color: red;
+    color: white;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue/dist/vue.js'
+import App from '@/App'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render(h){
+      return h(App)
+    }
+  }
+).$mount('#app')
+
+const x = 1
+if(x===1){
+  console.log(1)
+}
